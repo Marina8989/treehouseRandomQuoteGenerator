7f904f0c203f97298cb7c13cc862d9e3d0248eca
Marina8989 <marina.mill209@gmail.com>
2021-07-19 11:28:26 -0700

    added quote list

---
+++ main.js
@@ -0,0 +1,43 @@
+const data = [
+    {
+        quote: 'Success is not final; failure is not fatal: It is the courage to continue that counts.',
+        author: '- Winston S. Churchill',
+        place: 'Google'
+    },
+    {
+        quote: 'It is better to fail in originality than to succeed in imitation.',
+        author: '- Herman Melville',
+        place: 'Google'
+    },
+    {
+        quote: 'The road to success and the road to failer are almost exactly the same.',
+        author: '- Colin R. Davis',
+        place: 'Google'
+    },
+    {
+        quote: 'Opportunities don\'t happen. You create them.',
+        author: '- Chris Grosser',
+        place: 'Google'
+    },
+    {
+        quote: 'Success is not final; failure is not fatal: It is the courage to continue that counts.',
+        author: '- Winston S. Churchill',
+        place: 'Google'
+    },
+    {
+        quote: 'Try not to become a man of success. Rather become a man of value.',
+        author: '- Albert Einstein',
+        place: 'Google'
+    },
+    {
+        quote: 'Stop chasing the money and start chasing the passion.',
+        author: '- Tony Hsieh',
+        place: 'Google'
+    },
+    {
+        quote: 'I owe my success to having listened respectfully to the very best advice, and then going away and doing the exact opposite.',
+        author: '- G.K. Chesterton',
+        place: 'Google'
+    },
+]
+
